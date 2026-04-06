3f10706df406c33600132a71e23ede6ca4a0b923
Jon Uhlmann <account@uhlmann.pro>
2026-04-06 11:05:17 +0200

    Upgrade: Packages

--- build.mjs
+++ build.mjs
@@ -1,17 +1,17 @@
 import esbuild from "esbuild";
 import extensibilityMap from "@neos-project/neos-ui-extensibility/extensibilityMap.json" with { type: "json" };
-import stylexPlugin from "@stylexjs/esbuild-plugin";
-import path from "path";
-import { fileURLToPath } from "url";
+import stylex from "@stylexjs/unplugin";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const watch = process.argv.includes("--watch");
+const dev = process.argv.includes("--dev");
+const minify = !dev && !watch;
 
 /** @type {import("esbuild").BuildOptions} */
 const options = {
     logLevel: "info",
     bundle: true,
-    minify: true,
-    sourcemap: true,
+    minify,
+    sourcemap: watch,
     target: "es2020",
     format: "esm",
     splitting: true,
@@ -24,18 +24,26 @@ const options = {
     },
     outdir: "Resources/Public",
     alias: extensibilityMap,
+    metafile: true,
     plugins: [
-        stylexPlugin({
-            classNamePrefix: "tailwindspacing-",
+        stylex.esbuild({
             useCSSLayers: false,
-            dev: false,
-            generatedCSSFileName: path.resolve(__dirname, "Resources/Public/Plugin.css"),
-            stylexImports: ["@stylexjs/stylex"],
+            classNamePrefix: "tailwindspacing-",
+            dev,
+            lightningcssOptions: {
+                minify,
+            },
         }),
     ],
 };
 
-if (process.argv.includes("--watch")) {
+if (minify) {
+    options.drop = ["debugger"];
+    options.pure = ["console.log"];
+    options.dropLabels = ["DEV"];
+}
+
+if (watch) {
     esbuild.context(options).then((ctx) => ctx.watch());
 } else {
     esbuild.build(options);
